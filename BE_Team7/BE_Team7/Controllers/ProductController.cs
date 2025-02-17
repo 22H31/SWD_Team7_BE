70dea0f36065c409198aed0fab09b58f36ab3e44
22H31 <quangdmse170316@fpt.edu.vn>
2025-02-18 00:28:19 +0700

    geting start

---
+++ BE_Team7/BE_Team7/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using BE_Team7.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_Team7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> products = new List<Product>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductIf productIf) 
+        {
+            var product1 = new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = productIf.Name,
+                Description = productIf.Description,
+                CategoryId = productIf.CategoryId,    
+                Price = productIf.Price,
+                SkinType = productIf.SkinType,
+                StockQuantity = productIf.StockQuantity,
+                CreatedAt = productIf.CreatedAt
+            };
+            products.Add(product1);
+            return Ok(new
+            {
+                Success = true, Data = product1
+            });
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByProductId(string id) 
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var product2 = products.SingleOrDefault(hh => hh.ProductId == Guid.Parse(id));
+                if (product2 == null)
+                {
+                    return NotFound();
+                }
+                return Ok(product2);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditProduct(string id, Product productEdit) 
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var product3 = products.SingleOrDefault(hh => hh.ProductId == Guid.Parse(id));
+                if (product3 == null)
+                {
+                    return NotFound();
+                }
+
+                if (id != product3.ProductId.ToString()) 
+                {
+                    return BadRequest();
+                }
+                // edit
+                product3.Name = productEdit.Name;
+                product3.Description = productEdit.Description;
+                product3.CategoryId = productEdit.CategoryId;
+                product3.Price = productEdit.Price;
+                product3.SkinType = productEdit.SkinType;
+                product3.StockQuantity = productEdit.StockQuantity;
+                product3.CreatedAt = productEdit.CreatedAt;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(string id)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var product4 = products.SingleOrDefault(hh => hh.ProductId == Guid.Parse(id));
+                if (product4 == null)
+                {
+                    return NotFound();
+                }
+                products.Remove(product4);
+                return Ok(new
+                {
+                    Success = true,
+                    Data = products.ToArray()
+                });
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ BE_Team7/BE_Team7/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using BE_Team7.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_Team7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.User.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
